16ae105205535b6144e432048249c43ed43b8e78
lucasDev88 <lucasfelipe300810@gmail.com>
2026-09-11 22:44:24 -0300

    fix: await bcrypt password comparison

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -108,7 +108,7 @@ export async function login(req: Request, res: Response) {
             );
         }
 
-        const passwordMatch = bcrypt.compare(password, user.password)
+        const passwordMatch = await bcrypt.compare(password, user.password)
 
         if (!passwordMatch) {
             return res.status(401).json(
